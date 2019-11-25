8afb20545d30032f96209b7a5633593a97ceaa34
John W. Fail <john@pixelache.ac>
2019-11-25 21:10:32 +0200

    hardcode omniauth callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.facebook[:client_id], Rails.application.credentials.facebook[:secret]
+  provider :facebook, Rails.application.credentials.facebook[:client_id], Rails.application.credentials.facebook[:secret], callback_url: Rails.env.development? ? 'http://localhost:3000/omniauth/facebook/callback' :  'https://api.freeriga.lv/omniauth/facebook/callback'
 end
